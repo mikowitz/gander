2d8d8e69808598f6901de8bb96bffb710c0341ed
Michael Berkowitz <michael.berkowitz@gmail.com>
2026-03-27 15:37:39 -0400

    Implement InsertLeft and InsertRight

--- edit.go
+++ edit.go
@@ -15,3 +15,27 @@ func Edit(z Zipper, f func(Node) Node) (Zipper, bool) {
 	focus := f(z.focus)
 	return Replace(z, focus)
 }
+
+func InsertLeft(z Zipper, n Node) (Zipper, bool) {
+	if z.path == nil {
+		return z, false
+	}
+	z.path.left = append(z.path.left, n)
+	z.path.changed = true
+	return Zipper{
+		focus: z.focus,
+		path:  z.path,
+	}, true
+}
+
+func InsertRight(z Zipper, n Node) (Zipper, bool) {
+	if z.path == nil {
+		return z, false
+	}
+	z.path.right = append([]Node{n}, z.path.right...)
+	z.path.changed = true
+	return Zipper{
+		focus: z.focus,
+		path:  z.path,
+	}, true
+}
